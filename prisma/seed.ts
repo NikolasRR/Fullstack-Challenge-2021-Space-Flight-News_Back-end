79c5e3afd810d00e9db407b1b04b5400bfdc7ae9
Nikolas <nkls99@gmail.com>
2022-11-08 22:11:19 -0300

    docker enabled

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,8 +7,7 @@ import saveNewsFromAPI from "../src/CRUD/utils.js";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const countResponse = await axios.get(`${process.env.SPACENEWS_URL}/count`);
-  const countResponse = {data:50};
+  const countResponse = await axios.get(`${process.env.SPACENEWS_URL}/count`);
 
   const articlesResponse = await axios.get(`${process.env.SPACENEWS_URL}?_limit=${countResponse.data}`);
   const articles: RawArticle[] = articlesResponse.data;
